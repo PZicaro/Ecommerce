c3fee9e1044af3d6b4c91e1d7a16e23778ce29ea
Ícaro Rodrigues <icaro2rodrigues@gmail.com>
2023-01-15 00:24:01 -0300

    fix: change the div-container to fragment

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -3,11 +3,10 @@ import Header from '../../components/Header/header.component'
 
 const Home = () => {
   return (
-        <div>
+        <>
             <Header/>
            <Categories/>
-
-        </div>
+        </>
   )
 }
 export default Home
