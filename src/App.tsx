b584b18c2e0870838c0b39c792b55c490e7c80ed
Ícaro Rodrigues <96674451+PZicaro@users.noreply.github.com>
2023-02-28 19:25:25 -0300

    feat: create user Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import { onAuthStateChanged } from 'firebase/auth'
-import { FunctionComponent } from 'react'
+import { collection, getDocs, query, where } from 'firebase/firestore'
+import { FunctionComponent, useContext } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { auth } from './config/firebase.config'
+import { auth, db } from './config/firebase.config'
+import { UserContext } from './context/user.contex'
 import Home from './pages/home/home.page'
 import LoginPage from './pages/login/login.page'
 import SignUpPage from './pages/sign-up/sign-up.page'
 
 const App: FunctionComponent = () => {
-  onAuthStateChanged(auth, (user) => {
-    console.log(user)
+  const { isAuthenticated, loginUser, logoutUser } = useContext(UserContext)
+  console.log(isAuthenticated)
+  onAuthStateChanged(auth, async (user) => {
+    const isSigningOut = isAuthenticated && !user
+    // eslint-disable-next-line space-before-blocks
+    if (isSigningOut){
+      return logoutUser()
+    }
+    const isSignIn = !isAuthenticated && user
+    if (isSignIn) {
+      const querySnapshot = await getDocs(query(collection(db, 'users'), where('id', '==', user.uid)))
+      const userFromFiresStore = querySnapshot.docs[0]?.data()
+      return loginUser(userFromFiresStore as any)
+    }
   })
   return (
    <BrowserRouter>
---
+++ src/context/user.contex.tsx
@@ -0,0 +1,38 @@
+import React, { createContext, FunctionComponent, useState } from 'react'
+import User from '../types/user.types'
+
+interface userContextProviderProps {
+  children: React.ReactNode
+}
+interface IUserContext {
+  currentUser: User | null
+  isAuthenticated: boolean
+  loginUser: (user: User) => void
+  logoutUser: () => void
+
+}
+
+export const UserContext = createContext <IUserContext>({
+  currentUser: null,
+  isAuthenticated: false,
+  loginUser: () => {},
+  logoutUser: () => {}
+})
+
+const UserContextProvider:FunctionComponent <userContextProviderProps> = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState<User | null>(null)
+  const isAuthenticated = currentUser !== null
+  const loginUser = (user: User) => {
+    setCurrentUser(user)
+  }
+  const logoutUser = () => {
+    setCurrentUser(null)
+  }
+  return (
+  <UserContext.Provider value = {{ currentUser, isAuthenticated, loginUser, logoutUser }}>
+   {children}
+  </UserContext.Provider>
+  )
+}
+
+export default UserContextProvider
