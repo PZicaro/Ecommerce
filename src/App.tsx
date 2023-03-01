a2ae3b4960962ae8e14422406e00f2e632a40ea4
Ícaro Rodrigues <96674451+PZicaro@users.noreply.github.com>
2023-03-01 02:28:12 -0300

    feat: add initializing state to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { onAuthStateChanged } from 'firebase/auth'
 import { collection, getDocs, query, where } from 'firebase/firestore'
-import { FunctionComponent, useContext } from 'react'
+import { FunctionComponent, useContext, useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { auth, db } from './config/firebase.config'
 import { UserContext } from './context/user.contex'
@@ -9,21 +9,26 @@ import LoginPage from './pages/login/login.page'
 import SignUpPage from './pages/sign-up/sign-up.page'
 
 const App: FunctionComponent = () => {
+  const [isInitializing, setisInitializing] = useState(true)
   const { isAuthenticated, loginUser, logoutUser } = useContext(UserContext)
   console.log(isAuthenticated)
   onAuthStateChanged(auth, async (user) => {
     const isSigningOut = isAuthenticated && !user
     // eslint-disable-next-line space-before-blocks
     if (isSigningOut){
-      return logoutUser()
+      logoutUser()
+      return setisInitializing(false)
     }
     const isSignIn = !isAuthenticated && user
     if (isSignIn) {
       const querySnapshot = await getDocs(query(collection(db, 'users'), where('id', '==', user.uid)))
       const userFromFiresStore = querySnapshot.docs[0]?.data()
-      return loginUser(userFromFiresStore as any)
+      loginUser(userFromFiresStore as any)
+      return setisInitializing(false)
     }
+    return setisInitializing(false)
   })
+  if (isInitializing) return null
   return (
    <BrowserRouter>
     <Routes>
